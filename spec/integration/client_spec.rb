7cc666f42111b36265426e6735afbb40ee6649a6
Bill Siggelkow <bill.siggelkow@blinqmedia.com>
2014-11-11 09:54:11 -0500

    Add integration spec

--- spec/integration/client_spec.rb
+++ spec/integration/client_spec.rb
@@ -1,3 +1,28 @@
 require 'spec_helper'
+require 'calculator_service'
+require 'calculator_server'
+
 describe 'client calls service' do
+
+  # Run the server in a forked sub-process
+  def launch_server
+    pid = fork do
+      puts "Starting server ..."
+      server = Calculator::Server.new
+      server.serve
+    end
+  end
+
+  before(:each) do
+    launch_server
+    transport = ThriftyBunny::ClientTransport.new
+    protocol = Thrift::BinaryProtocol.new(transport)
+    @client = CalculatorService::Client.new(protocol)
+  end
+
+  it 'works' do
+    res = @client.say_hello('Bob')
+    expect(res).to eq("Hello, Bob from Thrift!")
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
-$LOAD_PATH.unshift File.expand_path("../lib", __FILE__)
-$LOAD_PATH.unshift File.expand_path("../examples", __FILE__)
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+$LOAD_PATH.unshift File.expand_path("../../examples/calculator", __FILE__)
 
 require 'bundler'
 Bundler.setup(:default, :test)
