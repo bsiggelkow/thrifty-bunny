0e7d8b184bdc59b77ecca5372587213fb5fac64b
Bill Siggelkow <bill.siggelkow@blinqmedia.com>
2015-01-05 12:05:19 -0500

    Add exception to .thrift file

--- examples/calculator/calculator_service.thrift
+++ examples/calculator/calculator_service.thrift
@@ -1,6 +1,11 @@
+exception DivideByZeroException {
+  1: string message
+}
+
 service CalculatorService {
   string say_hello(1: string name),
   i32 add(1: i32 value1, 2: i32 value2),
-  double divide(1: i32 dividend, 2: i32 divisor),
+  double divide(1: i32 dividend, 2: i32 divisor)
+    throws (1: DivideByZeroException),
   void ping()
 }
